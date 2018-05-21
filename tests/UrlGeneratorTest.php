92d4cc2c069be6c7fd9930ca60765d1495aa128f
Cyril de Wit <cyrilwit@gmail.com>
2018-05-21 21:29:06 +0200

    test: UrlGenerator

---
+++ tests/UrlGeneratorTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CyrildeWit\MapsUrls\Tests;
+
+/*
+ * This file is part of the Maps URLs package.
+ *
+ * (c) Cyril de Wit <github@cyrildewit.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use PHPUnit\Framework\TestCase;
+use CyrildeWit\MapsUrls\Actions\ActionInterface;
+use CyrildeWit\MapsUrls\UrlGenerator;
+
+class UrlGeneratorTest extends TestCase
+{
+    public function testGenerate()
+    {
+        $urlGenerator = new UrlGenerator(new class implements ActionInterface {
+            public function getEndpoint(): string
+            {
+                return 'search/';
+            }
+
+            public function getParameters(): array
+            {
+                return [
+                    'test' => 'test',
+                    'foo' => 'bar',
+                ];
+            }
+        });
+
+        $this->assertEquals(
+            'https://www.google.com/maps/search/?api=1&test=test&foo=bar',
+            $urlGenerator->generate()
+        );
+    }
+}
