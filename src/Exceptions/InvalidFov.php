e49c8af4e6821cf3f95f95cc2e246dabd1090273
Cyril de Wit <cyril@cyrildewit.nl>
2026-08-29 13:35:17 +0200

    fix(exceptions): correct the ranges in out-of-range messages

--- src/Exceptions/InvalidFov.php
+++ src/Exceptions/InvalidFov.php
@@ -8,6 +8,6 @@ class InvalidFov extends Exception
 {
     public static function outOfRange(int $degrees): self
     {
-        return new static("Invalid fov provided. Expected from -10 to 100 degrees. Received '{$degrees}`");
+        return new static("Invalid fov provided. Expected from 10 to 100 degrees. Received '{$degrees}'.");
     }
 }
