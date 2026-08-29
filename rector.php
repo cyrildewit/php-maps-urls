34b664d3b8c3d6565fbb4d4d807ae2c6d5146945
Cyril de Wit <cyril@cyrildewit.nl>
2026-08-29 14:59:35 +0200

    build(rector): widen the rule sets

--- rector.php
+++ rector.php
@@ -4,20 +4,34 @@ declare(strict_types=1);
 
 use Pest\Rector\Set\PestSetList;
 use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
+    ->withRootFiles()
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
+        codingStyle: true,
         typeDeclarations: true,
+        typeDeclarationDocblocks: true,
         privatization: true,
+        namedArgs: true,
+        instanceOf: true,
+        if: true,
         phpunitCodeQuality: true,
+        phpunitNarrowAsserts: true,
+        phpunitMockToStub: true,
     )
+    ->withComposerBased(phpunit: true)
     ->withSets([
         PestSetList::CODING_STYLE,
     ])
-    ->withPhpSets();
+    ->withPhpSets()
+    ->withRules([
+        DeclareStrictTypesRector::class,
+    ])
+    ->withImportNames();
